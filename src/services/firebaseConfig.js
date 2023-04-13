b33054076af8f06120032a02c79f66848b4ce1ed
Nathan Marques de Aguiar Brandão <108840944+NathanMarques2001@users.noreply.github.com>
2023-04-13 10:38:49 -0300

    erro: variaveis de ambiente

--- src/services/firebaseConfig.js
+++ src/services/firebaseConfig.js
@@ -1,21 +1,15 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app'
-import { getAnalytics } from 'firebase/analytics'
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
 const firebaseConfig = {
-  apiKey: 'AIzaSyBg_APDa-Mx_OUOgeooR4ADbKN6mFplw4Q',
-  authDomain: 'britaldo-estoque.firebaseapp.com',
-  projectId: 'britaldo-estoque',
-  storageBucket: 'britaldo-estoque.appspot.com',
-  messagingSenderId: '260435782937',
-  appId: '1:260435782937:web:9a0413ebbcd4e31476e988',
-  measurementId: 'G-6C9K0QNTMQ',
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 }
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig)
-const analytics = getAnalytics(app)
--- src/services/recover.js
+++ src/services/recover.js
@@ -1,17 +1,17 @@
-function recoverPassword() {
-    showLoading();
-    firebase.auth().sendPasswordResetEmail(form.email().value).then(() => {
-        hideLoading();
-        alert('Email enviado com sucesso');
-    }).catch(error => {
-        hideLoading();
-        alert(getErrorMessage(error));
-    });
+import { getAuth, sendPasswordResetEmail } from 'firebase/auth'
+import { app } from './firebaseConfig.js'
+
+function recoverPassword(email) {
+  const auth = getAuth(app)
+  sendPasswordResetEmail(auth, email)
+    .then((response) => {
+      console.log('Email enviado com sucesso')
+    })
+    .catch((error) => {
+      if (error.code == 'auth/user-not-found') {
+        console.error('Usuário não encontrado')
+      }
+    })
 }
 
-function getErrorMessage(error) {
-    if (error.code == "auth/user-not-found"){
-        return "Usuário não encontrado";
-    }
-    return error.message;
-}
+recoverPassword("nathanbrandao1@gmail.com")
