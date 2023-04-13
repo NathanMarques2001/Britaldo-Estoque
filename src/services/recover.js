34ded19f704ba7ea1d8812d64d3cbe02c0374a63
fochatt <112530913+fochatt@users.noreply.github.com>
2023-04-13 04:54:04 -0300

    Recovery

--- src/services/recover.js
+++ src/services/recover.js
@@ -8,3 +8,10 @@ function recoverPassword() {
         alert(getErrorMessage(error));
     });
 }
+
+function getErrorMessage(error) {
+    if (error.code == "auth/user-not-found"){
+        return "Usuário não encontrado";
+    }
+    return error.message;
+}
