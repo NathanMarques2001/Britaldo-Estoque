07e8fa09e8d7a01662750fd40e159a408b4a04a3
fochatt <112530913+fochatt@users.noreply.github.com>
2023-04-13 04:40:29 -0300

    recoverPassword

---
+++ src/services/recover.js
@@ -0,0 +1,10 @@
+function recoverPassword() {
+    showLoading();
+    firebase.auth().sendPasswordResetEmail(form.email().value).then(() => {
+        hideLoading();
+        alert('Email enviado com sucesso');
+    }).catch(error => {
+        hideLoading();
+        alert(getErrorMessage(error));
+    });
+}
