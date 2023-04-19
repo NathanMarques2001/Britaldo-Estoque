bdca8004d7b9b0c5d73b9ee2bcc052193932428d
Nathan Marques de Aguiar Brandão <108840944+NathanMarques2001@users.noreply.github.com>
2023-04-19 10:40:36 -0300

    new features

--- src/services/firebaseConfig.js
+++ src/services/firebaseConfig.js
@@ -1,3 +1,17 @@
+import { initializeApp } from 'firebase/app'
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from "firebase/firestore";
+
+//Configurações basicas da aplicação
+const firebaseConfig = {
+  apiKey: "AIzaSyBg_APDa-Mx_OUOgeooR4ADbKN6mFplw4Q",
+  authDomain: "britaldo-estoque.firebaseapp.com",
+  projectId: "britaldo-estoque",
+  storageBucket: "britaldo-estoque.appspot.com",
+  messagingSenderId: "260435782937",
+  appId: "1:260435782937:web:9a0413ebbcd4e31476e988",
+  measurementId: "G-6C9K0QNTMQ",
+}
 
 //Inicializa o firebase
 export const app = initializeApp(firebaseConfig)
