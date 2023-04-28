6f1d997c913036e1b01e63597dc569fdcb44a801
NathanMarques2001 <nathanmarques20@yahoo.com.br>
2023-04-28 02:28:05 -0300

    add loading req firestore

--- src/components/tabela/index.jsx
+++ src/components/tabela/index.jsx
@@ -6,10 +6,12 @@ import { useEffect, useState } from 'react'
 import UsersCollection from '../../services/firestore/UsersCollection'
 import ProdutosCollection from '../../services/firestore/ProdutosCollection'
 import { traduzPermissao } from '../../utils/formataDados.js'
+import { Loading } from '../loading'
 
 export function Tabela({ titulo2, titulo3, tabela }) {
   const [produtos, setProdutos] = useState([])
   const [usuarios, setUsuario] = useState([])
+  const [loading, setLoading] = useState(false);
 
   const data = tabela === 'produtos' ? produtos : usuarios;
 
@@ -17,46 +19,49 @@ export function Tabela({ titulo2, titulo3, tabela }) {
   const produtosCollection = new ProdutosCollection();
 
   useEffect(() => {
+    setLoading(true)
     usersCollection.get(setUsuario);
     produtosCollection.get(setProdutos);
+    setLoading(false)
   }, [])
 
   return (
     <>
-    <table id="tabela-produtos">
-      <thead>
-        <tr>
-          <th>Nome</th>
-          <th>{titulo2}</th>
-          <th>{titulo3}</th>
-          <th>Ações</th>
-        </tr>
-      </thead>
-      <tbody>
-        {data.map((item, index) => (
-          <tr key={index}>
-            <td>{item.nome}</td>
-            <td>{item.quantidade || item.email}</td>
-            <td>{item.observacoes || traduzPermissao(item.permissao)}</td>
-            <td id="container-botao-tabela">
-              {
-                tabela === 'produtos' ? <button id="btn-baixa" className="botao-tabela">
-                  <img src={Baixa} alt="" className="img-botao" id="img-baixa" />
-                </button> : <></>
-              }
-              <button id="btn-editar" className="botao-tabela">
-                <img src={Editar} alt="" className="img-botao" id="img-editar" />
-              </button>
-              {
-              item.permissao === 'Superadmin' ? <></> : <button id="btn-excluir" className="botao-tabela">
-                <img src={Excluir} alt="" className="img-botao" id="img-excluir" />
-              </button>
-              }
-            </td>
+      {loading ? <Loading /> : <> </>}
+      <table id="tabela-produtos">
+        <thead>
+          <tr>
+            <th>Nome</th>
+            <th>{titulo2}</th>
+            <th>{titulo3}</th>
+            <th>Ações</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {data.map((item, index) => (
+            <tr key={index}>
+              <td>{item.nome}</td>
+              <td>{item.quantidade || item.email}</td>
+              <td>{item.observacoes || traduzPermissao(item.permissao)}</td>
+              <td id="container-botao-tabela">
+                {
+                  tabela === 'produtos' ? <button id="btn-baixa" className="botao-tabela">
+                    <img src={Baixa} alt="" className="img-botao" id="img-baixa" />
+                  </button> : <></>
+                }
+                <button id="btn-editar" className="botao-tabela">
+                  <img src={Editar} alt="" className="img-botao" id="img-editar" />
+                </button>
+                {
+                  item.permissao === 'Superadmin' ? <></> : <button id="btn-excluir" className="botao-tabela">
+                    <img src={Excluir} alt="" className="img-botao" id="img-excluir" />
+                  </button>
+                }
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </>
   )
 }
--- src/services/firestore/user.js
+++
@@ -1,7 +0,0 @@
-import { doc, setDoc } from "firebase/firestore";
-import { db } from "../firebaseConfig";
-
-
-export function post(dados, uid) {
-    setDoc(doc(db, 'users', uid), dados)
-}
